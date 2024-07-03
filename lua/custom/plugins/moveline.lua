d33472a7cd7f1b9b4f94b2363eff8c439b53a9cc
thederpykrafter <thederpykrafter@gmail.com>
2024-07-03 15:46:11 -0400

    moveline

--- lua/custom/plugins/moveline.lua
+++ lua/custom/plugins/moveline.lua
@@ -1,5 +1,7 @@
 -- move line of text
-keymap("n", "<A-j>", "<cmd>m-2<cr>", { desc = "Move Line Down"})
-keymap("v", "<A-j>", "ddjp", { desc = "Move Line Down"})
-keymap("n", "<A-k>", "<cmd>m+<cr>", { desc = "Move Line Up"})
-keymap("v", "<A-k>", "ddkp", { desc = "Move Line Up"})
+local keymap = vim.keymap.set
+
+keymap('n', '<A-j>', '<cmd>m-2<cr>', { desc = 'Move Line Down' })
+keymap('v', '<A-j>', 'ddjp', { desc = 'Move Line Down' })
+keymap('n', '<A-k>', '<cmd>m+<cr>', { desc = 'Move Line Up' })
+keymap('v', '<A-k>', 'ddkp', { desc = 'Move Line Up' })
