72fe149733e5f4089ba5eff62aed24d6ac0b7678
thederpykrafter <thederpykrafter@gmail.com>
2024-08-08 18:33:57 -0400

    oil

--- init.lua
+++ init.lua
@@ -78,6 +78,11 @@ vim.opt.breakindent = true
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+-- Oil
+vim.keymap.set('n', '<leader>e', '<cmd>Oil<cr>', { desc = 'Oil' })
+-- open config with Oil
+vim.keymap.set('n', '<leader>c', '<cmd> Oil ~/.config/nvim<cr>', { desc = 'Oil - NeoVim Config' })
+
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
@@ -85,7 +90,7 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>E', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,10 @@
+return {
+  'stevearc/oil.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  opts = {
+    keymaps = {
+      ['q'] = 'actions.close',
+      ['H'] = 'actions.toggle_hidden',
+    },
+  },
+}
