192a7f112107a3ba1643c114ba0d512f28b6eaf8
thederpykrafter <thederpykrafter@gmail.com>
2024-08-09 02:38:01 -0400

    c3 parser issue?

--- init.lua
+++ init.lua
@@ -798,6 +798,23 @@ require('lazy').setup({
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup(opts)
 
+      vim.filetype.add {
+        extension = {
+          c3 = 'c3',
+          c3i = 'c3',
+          c3t = 'c3',
+        },
+      }
+
+      ---@class parser_config . [inject-field]
+      local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+      parser_config.c3 = {
+        install_info = {
+          url = 'https://github.com/c3lang/tree-sitter-c3',
+          files = { 'src/parser.c', 'src/scanner.c' },
+          branch = 'main',
+        },
+      }
       -- There are additional nvim-treesitter modules that you can use to interact
       -- with nvim-treesitter. You should go explore a few and see what interests you:
       --
