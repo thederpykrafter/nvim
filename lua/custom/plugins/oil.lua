c4e9907718d0808f54a8ceb73a3055db109c3ab2
thederpykrafter <thederpykrafter@gmail.com>
2025-01-20 16:25:42 -0500

    oil changes

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -2,6 +2,8 @@ return {
   'stevearc/oil.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {
+    skip_confirm_for_simple_edits = true,
+    watch_for_changes = true,
     keymaps = {
       ['q'] = 'actions.close',
       ['H'] = 'actions.toggle_hidden',
