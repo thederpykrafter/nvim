90c2576a839a17c8a107941a6cb206ebf12a8409
thederpykrafter <thederpykrafter@gmail.com>
2025-05-20 23:20:26 -0400

    lsp changes

--- init.lua
+++ init.lua
@@ -337,11 +337,11 @@ require("lazy").setup({
 	{
 		"neovim/nvim-lspconfig",
 		dependencies = {
-			{ "williamboman/mason.nvim", config = true },
-			"williamboman/mason-lspconfig.nvim",
+			{ "mason-org/mason.nvim", config = true },
+			"mason-org/mason-lspconfig.nvim",
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
 			{ "j-hui/fidget.nvim", opts = {} },
-			{ "folke/neodev.nvim", opts = {} },
+			"saghen/blink.cmp",
 		},
 		config = function()
 			vim.api.nvim_create_autocmd("LspAttach", {
@@ -411,11 +411,40 @@ require("lazy").setup({
 						"[G]oto [D]eclaration"
 					)
 
+					-- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
+					---@param client vim.lsp.Client
+					---@param method vim.lsp.protocol.Method
+					---@param bufnr? integer some lsp support methods only in specific files
+					---@return boolean
+					local function client_supports_method(
+						client,
+						method,
+						bufnr
+					)
+						if vim.fn.has "nvim-0.11" == 1 then
+							return client:supports_method(method, bufnr)
+						else
+							return client.supports_method(
+								method,
+								{ bufnr = bufnr }
+							)
+						end
+					end
+
+					-- The following two autocommands are used to highlight references of the
+					-- word under your cursor when your cursor rests there for a little while.
+					--    See `:help CursorHold` for information about when this is executed
+					--
+					-- When you move your cursor, the highlights will be cleared (the second autocommand).
 					local client =
 						vim.lsp.get_client_by_id(event.data.client_id)
 					if
 						client
-						and client.server_capabilities.documentHighlightProvider
+						and client_supports_method(
+							client,
+							vim.lsp.protocol.Methods.textDocument_documentHighlight,
+							event.buf
+						)
 					then
 						local highlight_augroup = vim.api.nvim_create_augroup(
 							"kickstart-lsp-highlight",
@@ -454,16 +483,25 @@ require("lazy").setup({
 						})
 					end
 
+					-- The following code creates a keymap to toggle inlay hints in your
+					-- code, if the language server you are using supports them
+					--
+					-- This may be unwanted, since they displace some of your code
 					if
 						client
-						and client.server_capabilities.inlayHintProvider
-						and vim.lsp.inlay_hint
+						and client_supports_method(
+							client,
+							vim.lsp.protocol.Methods.textDocument_inlayHint,
+							event.buf
+						)
 					then
 						map(
 							"<leader>th",
 							function()
 								vim.lsp.inlay_hint.enable(
-									not vim.lsp.inlay_hint.is_enabled()
+									not vim.lsp.inlay_hint.is_enabled {
+										bufnr = event.buf,
+									}
 								)
 							end,
 							"[T]oggle Inlay [H]ints"
@@ -471,6 +509,35 @@ require("lazy").setup({
 					end
 				end,
 			})
+
+			-- Diagnostic Config
+			-- See :help vim.diagnostic.Opts
+			vim.diagnostic.config {
+				severity_sort = true,
+				float = { border = "rounded", source = "if_many" },
+				underline = { severity = vim.diagnostic.severity.ERROR },
+				signs = vim.g.have_nerd_font and {
+					text = {
+						[vim.diagnostic.severity.ERROR] = "󰅚 ",
+						[vim.diagnostic.severity.WARN] = "󰀪 ",
+						[vim.diagnostic.severity.INFO] = "󰋽 ",
+						[vim.diagnostic.severity.HINT] = "󰌶 ",
+					},
+				} or {},
+				virtual_text = {
+					source = "if_many",
+					spacing = 2,
+					format = function(diagnostic)
+						local diagnostic_message = {
+							[vim.diagnostic.severity.ERROR] = diagnostic.message,
+							[vim.diagnostic.severity.WARN] = diagnostic.message,
+							[vim.diagnostic.severity.INFO] = diagnostic.message,
+							[vim.diagnostic.severity.HINT] = diagnostic.message,
+						}
+						return diagnostic_message[diagnostic.severity]
+					end,
+				},
+			}
 
 			local capabilities =
 				vim.lsp.protocol.make_client_capabilities()
