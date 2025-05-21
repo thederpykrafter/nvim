aee99af88d1ce41add5eeaf45196adf1452aff64
thederpykrafter <thederpykrafter@gmail.com>
2025-05-20 23:18:54 -0400

    add lazydev

--- init.lua
+++ init.lua
@@ -322,6 +322,19 @@ require("lazy").setup({
 	},
 
 	{
+		-- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+		-- used for completion, annotations and signatures of Neovim apis
+		"folke/lazydev.nvim",
+		ft = "lua",
+		opts = {
+			library = {
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+			},
+		},
+	},
+
+	{
 		"neovim/nvim-lspconfig",
 		dependencies = {
 			{ "williamboman/mason.nvim", config = true },
